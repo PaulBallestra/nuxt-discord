81f228c1beb1bab37160d3e6e66a7646e19c42c8
PaulBallestra <paulballestra@gmail.com>
2025-12-12 00:55:55 +0100

    :sparkles: Added supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase'],
   css: [
     './app/assets/global.css'
   ],
@@ -12,4 +13,13 @@ export default defineNuxtConfig({
       tailwindcss(),
     ]
   },
+  supabase: {
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      include: undefined,
+      exclude: [],
+      saveRedirectToCookie: false,
+    }
+  }
 })
