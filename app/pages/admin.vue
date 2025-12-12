5c4b579e4e78ca83ca1d07c92dc4bf4560979310
PaulBallestra <paulballestra@gmail.com>
2025-12-12 03:03:22 +0100

    :sparkles: Admin connected session & user infos

--- app/pages/admin.vue
+++ app/pages/admin.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-    definePageMeta({
-        layout: 'admin-nav'
-    })
+definePageMeta({
+    layout: 'admin-nav'
+})
+const session = useSupabaseSession()
+const user = useSupabaseUser()
 </script>
 
 <template>
     Admin
+    <br>
+    Session
+    {{ session }}
+    <br>
+    User
+    {{ user }}
 </template>
